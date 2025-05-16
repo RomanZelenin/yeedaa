cb2c51fc4c255092ab4847c170875eac0cf60a0a
Roman Zelenin <roman_zelenin@hotmail.com>
2025-05-16 16:16:26 +0300

    feat(*): add verification failed modal dialog

---
+++ src/app/pages/Login/VerificationFailedModal.tsx
@@ -0,0 +1,62 @@
+import {
+    IconButton,
+    Image,
+    Link,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalOverlay,
+    Text,
+    useDisclosure,
+    VStack,
+} from '@chakra-ui/react';
+
+import { CloseInCircleIcon } from '~/common/components/Icons/CloseInCircleIcon';
+
+export const VerificationFailedModal = () => {
+    const { isOpen: isVisible, onClose } = useDisclosure({ defaultIsOpen: true });
+
+    const handleOnClickClose = () => {
+        onClose();
+    };
+
+    return (
+        <Modal onClose={handleOnClickClose} isOpen={isVisible} isCentered>
+            <ModalOverlay />
+            <ModalContent borderRadius='16px' width={{ base: '316px', lg: '396px' }}>
+                <ModalBody display='flex' flexDirection='column' p={0}>
+                    <IconButton
+                        m='24px'
+                        boxSize='24px'
+                        alignSelf='end'
+                        minW={0}
+                        onClick={handleOnClickClose}
+                        backgroundColor='transparent'
+                        icon={<CloseInCircleIcon boxSize='24px' />}
+                        aria-label='close'
+                    />
+                    <VStack spacing='32px' p='32px'>
+                        <Image
+                            boxSize={{ base: '108px', lg: '206px' }}
+                            src='/src/assets/images/verification-failed.svg'
+                        />
+                        <VStack spacing={0} textAlign='center'>
+                            <Text textStyle='text2xlLh8Bold' mb={{ base: '16px' }}>
+                                Упс! Что-то пошло не так
+                            </Text>
+                            <Text textStyle='textMdLh6Semibold' color='blackAlpha.700'>
+                                Ваша ссылка для верификации недействительна. Попробуйте
+                                зарегистрироваться снова.
+                            </Text>
+                        </VStack>
+
+                        <Text textAlign='center' textStyle='textXsLh4Normal' color='blackAlpha.700'>
+                            {`Остались вопросы? Свяжитесь `}
+                            <Link href='#' textDecoration='underline'>{`с\u00A0поддержкой`}</Link>
+                        </Text>
+                    </VStack>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+};
