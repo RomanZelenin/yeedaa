5dff46cf56278125cb733e0ab43b1dd4655d060e
Roman Zelenin <roman_zelenin@hotmail.com>
2025-05-21 13:10:25 +0300

    refactor(*): remove unnecessary recipe sorting

--- src/app/pages/Home/Sections/SectionRelevantKitchen.tsx
+++ src/app/pages/Home/Sections/SectionRelevantKitchen.tsx
@@ -1,4 +1,5 @@
 import { Box, Divider, GridItem, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { useMemo } from 'react';
 
 import {
     VegeterianKitchenCard,
@@ -11,10 +12,11 @@ import { useAppDispatch } from '~/store/hooks';
 
 export default function SectionRelevantKitchen() {
     const dispatcher = useAppDispatch();
-
     const category = useRandomCategory();
-    const subcategoriesIds = category?.subCategories?.map((subcategory) => subcategory._id);
-
+    const subcategoriesIds = useMemo(
+        () => category?.subCategories?.map((subcategory) => subcategory._id),
+        [category],
+    );
     const {
         data: recipes,
         isLoading,
@@ -27,67 +29,67 @@ export default function SectionRelevantKitchen() {
 
     if (isLoading) {
         dispatcher(setRelevantLoader(true));
-        return <Text>Loading...</Text>;
+        return null;
     }
-    if (isError || isSuccess) {
+    if (isError) {
         dispatcher(setRelevantLoader(false));
-        if (isError) {
-            dispatcher(
-                setAppError({ value: Error.SERVER, message: 'Попробуйте поискать снова попозже' }),
-            );
-            return <></>;
-        }
+        dispatcher(
+            setAppError({ value: Error.SERVER, message: 'Попробуйте поискать снова попозже' }),
+        );
+        return null;
     }
-
-    return (
-        <Box px={{ base: '16px', lg: '0px' }}>
-            <Divider mb={{ base: 0, lg: '24px' }} />
-            <SimpleGrid
-                columns={{ base: 1, lg: 4 }}
-                mb={{ base: '16px', lg: '24px' }}
-                rowGap='12px'
-            >
-                <GridItem colSpan={{ base: 1, xl: 2 }}>
-                    <Text
-                        fontSize={{ base: '24px', lg: '36px', xl: '48px' }}
-                        fontWeight='500'
-                        lineHeight={{ base: '32px', lg: '40px', xl: '48px' }}
-                    >
-                        {category?.title}
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={{ base: 1, lg: 3, xl: 2 }}>
-                    <Text
-                        fontSize={{ base: '14px', lg: '16px' }}
-                        color='rgba(0, 0, 0, 0.64)'
-                        lineHeight={{ base: '20px', lg: '24px' }}
-                        fontWeight='500'
-                    >
-                        {category?.description}
-                    </Text>
-                </GridItem>
-            </SimpleGrid>
-            <SimpleGrid
-                columns={{ base: 1, md: 3, xl: 4 }}
-                columnGap={{ base: '12px', lg: '16px', xl: '24px' }}
-                rowGap={{ base: '12px', lg: '16px', xl: '24px' }}
-            >
-                {recipes?.data?.slice(0, 2)?.map((recipe, i) => (
-                    <GridItem key={i} colSpan={{ xl: 1 }}>
-                        <VegeterianKitchenCard recipe={recipe} />
+    if (isSuccess) {
+        dispatcher(setRelevantLoader(false));
+        return (
+            <Box px={{ base: '16px', lg: '0px' }}>
+                <Divider mb={{ base: 0, lg: '24px' }} />
+                <SimpleGrid
+                    columns={{ base: 1, lg: 4 }}
+                    mb={{ base: '16px', lg: '24px' }}
+                    rowGap='12px'
+                >
+                    <GridItem colSpan={{ base: 1, xl: 2 }}>
+                        <Text
+                            fontSize={{ base: '24px', lg: '36px', xl: '48px' }}
+                            fontWeight='500'
+                            lineHeight={{ base: '32px', lg: '40px', xl: '48px' }}
+                        >
+                            {category?.title}
+                        </Text>
                     </GridItem>
-                ))}
+                    <GridItem colSpan={{ base: 1, lg: 3, xl: 2 }}>
+                        <Text
+                            fontSize={{ base: '14px', lg: '16px' }}
+                            color='rgba(0, 0, 0, 0.64)'
+                            lineHeight={{ base: '20px', lg: '24px' }}
+                            fontWeight='500'
+                        >
+                            {category?.description}
+                        </Text>
+                    </GridItem>
+                </SimpleGrid>
+                <SimpleGrid
+                    columns={{ base: 1, md: 3, xl: 4 }}
+                    columnGap={{ base: '12px', lg: '16px', xl: '24px' }}
+                    rowGap={{ base: '12px', lg: '16px', xl: '24px' }}
+                >
+                    {recipes?.data?.slice(0, 2)?.map((recipe, i) => (
+                        <GridItem key={i} colSpan={{ xl: 1 }}>
+                            <VegeterianKitchenCard recipe={recipe} />
+                        </GridItem>
+                    ))}
 
-                <GridItem colSpan={{ xl: 2 }} display='flex' flexDirection='column'>
-                    <VStack spacing='12px' justify='space-between' align='stretch' flex={1}>
-                        {recipes?.data?.slice(2).map((recipe, i) => (
-                            <Box key={i}>
-                                <VegeterianKitchenCompactCard recipe={recipe} />
-                            </Box>
-                        ))}
-                    </VStack>
-                </GridItem>
-            </SimpleGrid>
-        </Box>
-    );
+                    <GridItem colSpan={{ xl: 2 }} display='flex' flexDirection='column'>
+                        <VStack spacing='12px' justify='space-between' align='stretch' flex={1}>
+                            {recipes?.data?.slice(2).map((recipe, i) => (
+                                <Box key={i}>
+                                    <VegeterianKitchenCompactCard recipe={recipe} />
+                                </Box>
+                            ))}
+                        </VStack>
+                    </GridItem>
+                </SimpleGrid>
+            </Box>
+        );
+    }
 }
