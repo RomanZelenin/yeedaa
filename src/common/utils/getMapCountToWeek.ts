93429c31fc7695ae7b19e719b19c527f7271fe20
Roman Zelenin <roman_zelenin@hotmail.com>
2025-06-28 19:29:30 +0300

    fix(count-statistics): change logic counting

--- src/common/utils/getMapCountToWeek.ts
+++ src/common/utils/getMapCountToWeek.ts
@@ -3,15 +3,21 @@ import { StatsItem } from '~/query/types';
 import { getStartOfWeek } from './getStartOfWeek';
 
 export function getMapCountToWeek(stats: StatsItem[]) {
-    const initialValue: { date: Date; count: number }[] = [];
+    const initialValue: { date: string; count: number }[] = [];
     return stats
         .map((it) => ({
             ...it,
             date: getStartOfWeek(new Date(it.date)),
         }))
+        .sort((a, b) => a.date.getTime() - b.date.getTime())
+        .map((it) => ({
+            ...it,
+            date: it.date.toLocaleString('en-US', { month: 'short', day: 'numeric' }),
+        }))
         .reduce((acc, curr) => {
             if (acc.length > 0 && acc[acc.length - 1].date === curr.date) {
                 return [
+                    ...acc.slice(0, acc.length - 1),
                     {
                         date: acc[acc.length - 1].date,
                         count: acc[acc.length - 1].count + curr.count,
@@ -21,10 +27,5 @@ export function getMapCountToWeek(stats: StatsItem[]) {
                 acc.push(curr);
                 return acc;
             }
-        }, initialValue)
-        .sort((a, b) => a.date.getTime() - b.date.getTime())
-        .map((it) => ({
-            ...it,
-            date: it.date.toLocaleString('en-US', { month: 'short', day: 'numeric' }),
-        }));
+        }, initialValue);
 }
